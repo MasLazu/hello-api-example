054b69cb7703b5c18ce1b7c0f48164d4cf42cc6b
mfaziz <mfaziz50@gmail.com>
2024-12-17 09:10:41 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Message: "Hello, World!",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("GET /hello", helloHandler)
+
+	port := getPort()
+
+	log.Println("Server starting on port ", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getPort() string {
+	if port := os.Getenv("PORT"); port != "" {
+		return port
+	}
+	return "8080"
+}
